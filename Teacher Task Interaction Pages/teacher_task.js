abbc897231709d158b92edadd3c8d2235cef12d3
Adel <lionadel1234@gmail.com>
2025-04-25 03:35:11 +0300

    handle of the taskid if not set

--- Teacher Task Interaction Pages/teacher_task.js
+++ Teacher Task Interaction Pages/teacher_task.js
@@ -1,4 +1,14 @@
 import { tasks, users } from "../tasks_data.js";
+
+let searchParams;
+let taskId;
+
+if (window.location.href.indexOf("?task_id=") == -1) {
+  window.location.href += "?task_id=" + "task-002";
+  searchParams = new URLSearchParams(document.location.search);
+  taskId = searchParams.get("task_id");
+}
+
 if (localStorage.getItem("Tasks") == null) {
   localStorage.setItem("Tasks", JSON.stringify(tasks));
 }
